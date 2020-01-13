4d9f3cf0b70ec32e37dbf62b0fbbcc6a58533d0b
Timofei1 <57501337+Timofei1@users.noreply.github.com>
2020-01-13 20:52:14 +0400

    Updates

---
+++ precache-manifest.45d9ad2846460150e56d87f3b5d7cb2c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87a2f166a992e6cf567f9898fa3b2daf",
+    "url": "/exp-pk/index.html"
+  },
+  {
+    "revision": "8dbd551d8b05eb67a372",
+    "url": "/exp-pk/static/css/2.2cfa1b23.chunk.css"
+  },
+  {
+    "revision": "8dbd551d8b05eb67a372",
+    "url": "/exp-pk/static/js/2.1f284822.chunk.js"
+  },
+  {
+    "revision": "85cbe00177b85c285c2e",
+    "url": "/exp-pk/static/js/main.73abade5.chunk.js"
+  },
+  {
+    "revision": "6453e1a40a1d9e3e2194",
+    "url": "/exp-pk/static/js/runtime-main.aa6a19c2.js"
+  },
+  {
+    "revision": "49e2f0ce226c4a43ffff80591ee3b075",
+    "url": "/exp-pk/static/media/persik.49e2f0ce.png"
+  },
+  {
+    "revision": "4de00b324e543b28e611fceaabbc00c7",
+    "url": "/exp-pk/static/media/persik1.4de00b32.png"
+  },
+  {
+    "revision": "d3dc3f55cc6ffd351b99679449b60162",
+    "url": "/exp-pk/static/media/persik2.d3dc3f55.png"
+  },
+  {
+    "revision": "67ee4bf74a2eeecd3e0fc75c5ee13c33",
+    "url": "/exp-pk/static/media/persik3.67ee4bf7.png"
+  },
+  {
+    "revision": "530116146947f8d447a7c39ce7bf56b6",
+    "url": "/exp-pk/static/media/persik4.53011614.png"
+  },
+  {
+    "revision": "bf0b65e089151c7526bb3a8a4fa7f3bb",
+    "url": "/exp-pk/static/media/persik5.bf0b65e0.png"
+  }
+]);
